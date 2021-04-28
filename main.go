fdf069960d546cbe4e93b8e1a074a63c638ab2fe
Kofo Okesola <okesolakofo@gmail.com>
2021-04-28 14:44:41 +0100

    converts to rpn

--- main.go
+++ main.go
@@ -2,33 +2,43 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 	"strings"
 	"unicode"
 )
 
-// we will be using this stack a lot
-type stack []string
-
-func (s stack) push(val string) {
-	s = append(s, val)
+var precedenceMap = map[string]int{
+	"/": 4,
+	"*": 3,
+	"+": 2,
+	"-": 1,
 }
 
-func (s stack) pop() (val string) {
-	if len(s) < 1 {
+// we will be using this stack a lot
+type stack struct {
+	slice []string
+}
+
+func (s *stack) push(val string) {
+	s.slice = append(s.slice, val)
+}
+
+func (s *stack) pop() (val string) {
+	if len(s.slice) < 1 {
 		return ""
 	}
-	val = s[len(s)-1]
-	s = s[:len(s)-1]
+	val = s.slice[len(s.slice)-1]
+	s.slice = s.slice[:len(s.slice)-1]
 	return
 }
 
-func (s stack) peek() string {
-	if len(s) < 1 {
+func (s *stack) peek() string {
+	if len(s.slice) < 1 {
 		return ""
 	}
-	return s[len(s)-1]
+	return s.slice[len(s.slice)-1]
 }
 
 var expression string
@@ -62,5 +72,38 @@ func main() {
 	if str := lastDigits.String(); str != "" {
 		tokens = append(tokens, str)
 	}
-	fmt.Println(tokens)
+
+	convertToRPN(tokens)
+}
+
+// support bracket
+func convertToRPN(tokens []string) *stack {
+	reg := regexp.MustCompile(`[+\*\/\-\(\)]`)
+	rpn := stack{[]string{}}
+	operators := stack{[]string{}}
+	for _, token := range tokens {
+		if _, err := strconv.Atoi(token); err == nil {
+			rpn.push(token)
+		} else {
+			if !reg.MatchString(token) {
+				panic("invalid expression")
+			}
+			if operators.peek() == "" {
+				operators.push(token)
+				continue
+			}
+
+			// check if previous operator takes precedence
+			prev := operators.peek()
+			for precedenceMap[prev] > precedenceMap[token] && prev != "" {
+				rpn.push(operators.pop())
+				prev = operators.peek()
+			}
+			operators.push(token)
+		}
+	}
+	for tok := operators.peek(); tok != ""; tok = operators.peek() {
+		rpn.push(operators.pop())
+	}
+	return &rpn
 }
